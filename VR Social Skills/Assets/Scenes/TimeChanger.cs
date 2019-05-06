9f7e358bba17ef9ad5c0b8fb6ebf5b5961a49257
AceDominion <43331877+AceDominion@users.noreply.github.com>
2019-05-06 15:59:38 +1000

    path directing user given an ending
    the path directing the user leads into a classroom

--- VR Social Skills/Assets/Scenes/TimeChanger.cs
+++ VR Social Skills/Assets/Scenes/TimeChanger.cs
@@ -44,6 +44,29 @@ public class TimeChanger : MonoBehaviour
                 text.text = "Time's up, game over";
             }
         }
+
+        if (path == 1 && timeTwo < 0)
+        {
+            if (time < 90)
+            {
+                text.text = "Down the corridor, around the corner and first door on the left";
+            }
+
+            if (time < 45)
+            {
+                text.text = "Time is running out, pleace go down the corridor around the corner and first door on the left";
+            }
+
+            if (time < 0)
+            {
+                text.text = "Time's up, game over";
+            }
+        }
+
+        if (path == 2)
+        {
+            text.text = "This is the classroom";
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -51,15 +74,21 @@ public class TimeChanger : MonoBehaviour
         if (other.gameObject.tag == "RD")
         {
             path = 1;
+            time = 90f;
         }
 
-        if (other.gameObject.tag != "RD")
+        if (other.gameObject.tag == "WD")
         {
             if (path == 0)
             {
                 text.text = "Wrong door please try again";
                 timeTwo = 1.5f;
             }
+        }
+
+        if (other.gameObject.tag == "RD2")
+        {
+            path = 2;
         }
     }
 }
