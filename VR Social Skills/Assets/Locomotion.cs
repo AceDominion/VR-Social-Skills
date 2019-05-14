30163b3b7e95790b2c045347bdc5b460ab0b1801
haydenj104 <50032860+haydenj104@users.noreply.github.com>
2019-05-14 11:34:53 +1000

    working on VR moment
    various things, got some VR movement working in a simulator, fixed some things that broke because of that, next step is to test in lab.

---
+++ VR Social Skills/Assets/Locomotion.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.Vive;
+
+public class Locomotion : MonoBehaviour
+{
+
+    public GameObject rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (ViveInput.GetPress(HandRole.RightHand, ControllerButton.DPadUp))
+        {
+            Debug.Log("Press DpadUp detected");
+            Vector2 xInput = ViveInput.GetPadPressAxis(HandRole.RightHand);
+
+        }
+
+        if (ViveInput.GetPress(HandRole.RightHand, ControllerButton.DPadDown))
+        {
+            Debug.Log("Press DpadDown detected");
+        }
+
+        if (ViveInput.GetPress(HandRole.RightHand, ControllerButton.DPadLeft))
+        {
+            Debug.Log("Press DpadLeft detected");
+        }
+
+        if (ViveInput.GetPress(HandRole.RightHand, ControllerButton.DPadRight))
+        {
+            Debug.Log("Press DpadRight detected");
+        }
+
+
+
+
+    }
+}
