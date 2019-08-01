34e5841c8152cf586844d45256ed607b2c542cf5
AceDominion <acedominion1@gmail.com>
2019-08-01 13:15:14 +1000

    movement almost done
    can move in direction facing, only need the D-pad reading now

--- VR Social Skills/Assets/Locomotion.cs
+++ VR Social Skills/Assets/Locomotion.cs
@@ -10,14 +10,11 @@ namespace UnityStandardAssets.Characters.FirstPerson
     {
 
         public GameObject rig;
-        public float thrust = 0.5f;
+        public float thrust = 10f;
         public int count;
         public float xF = 0;
         public float zF = 0;
-        public float zRo;
-        public float xRo;
-        public float Track;
-        public float TrackTwo;
+
 
         public Camera m_Camera;
 
@@ -30,12 +27,17 @@ namespace UnityStandardAssets.Characters.FirstPerson
         // Update is called once per frame
         void Update()
         {
+            zF = thrust * Mathf.Cos(m_Camera.transform.eulerAngles.y * Mathf.Deg2Rad);
+            xF = thrust * Mathf.Sin(m_Camera.transform.eulerAngles.y * Mathf.Deg2Rad);
 
+            rig.gameObject.GetComponent<Rigidbody>().AddForce(xF, 0, zF);
 
             if (ViveInput.GetPress(HandRole.RightHand, ControllerButton.DPadUp))
             {
                 Debug.Log("Press DpadUp detected");
                 count++;
+
+                rig.gameObject.GetComponent<Rigidbody>().AddForce(xF, 0, zF);
 
                 //Vector2 xInput = ViveInput.GetPadPressAxis(HandRole.RightHand);
 
@@ -43,29 +45,26 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
             if (ViveInput.GetPress(HandRole.RightHand, ControllerButton.DPadDown))
             {
+                thrust = -thrust;
+                rig.gameObject.GetComponent<Rigidbody>().AddForce(-xF, 0, -zF);
                 Debug.Log("Press DpadDown detected");
+
             }
 
             if (ViveInput.GetPress(HandRole.RightHand, ControllerButton.DPadLeft))
             {
+
+                rig.gameObject.GetComponent<Rigidbody>().AddForce(thrust, 0, 0);
                 Debug.Log("Press DpadLeft detected");
             }
 
             if (ViveInput.GetPress(HandRole.RightHand, ControllerButton.DPadRight))
             {
+
+                rig.gameObject.GetComponent<Rigidbody>().AddForce(thrust, 0, 0);
                 Debug.Log("Press DpadRight detected");
             }
 
-            Track = Mathf.Abs(m_Camera.transform.rotation.y);
-
-            zRo = Mathf.Cos(Mathf.Abs(m_Camera.transform.rotation.y) + 180);
-            xRo = Mathf.Sin(Mathf.Abs(m_Camera.transform.rotation.y) + 270);
-
-
-            zF = Mathf.Abs(thrust) * Mathf.Cos(Mathf.Abs(m_Camera.transform.rotation.y));
-            xF = Mathf.Abs(thrust) * Mathf.Sin(Mathf.Abs(m_Camera.transform.rotation.y) + 90);
-
-            rig.gameObject.GetComponent<Rigidbody>().AddForce(xF, 0, zF);
 
         }
     }
